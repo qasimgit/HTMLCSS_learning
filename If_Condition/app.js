b74f63c2a2f7131181d8525a6745bf92bd75d59a
Ahmed Raza <razatechuc11@gmail.com>
2019-12-08 21:40:38 +0500

    Some javacript conditions applied for learning, practice for get Full Date in javascript

---
+++ If_Condition/app.js
@@ -0,0 +1,13 @@
+// var age = prompt("Welcome to the page");
+// console.log(Number(age));
+
+// if (Number(age) !== NaN) {
+//   if (age >= 18) {
+//     console.log(a)
+//     alert("Congrats! You are Eligible for this course");
+//   } else {
+//     alert("Sorry! you are not Eligible");
+//   }
+// }
+
+  console.log(window.document.getElementById("h1"))
---
+++ app.js
@@ -0,0 +1,9 @@
+var name = prompt("Your Name:");
+var CNIC = prompt("Your CNIC:");
+var Age = prompt("Your Age:");
+
+if (Age >= 18) {
+  alert("You are eligible to enroll");
+} else {
+  alert("You are not eligible to enroll");
+}
